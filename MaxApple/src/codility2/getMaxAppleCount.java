88ea9b95bb202b514103d713107591d1d119dfea
Divy Saxena <divysaxena2@gmail.com>
2018-09-04 23:04:09 +0530

    modified

--- MaxApple/src/codility2/getMaxAppleCount.java
+++ MaxApple/src/codility2/getMaxAppleCount.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 import java.util.List;
 
 
-
 public class getMaxAppleCount {
     /**
-     *
      * @param A array denoting the number of apples on each apple tree in the row
      * @param K the number of trees that Alice can choose when collecting
      * @param L the number of trees that Alice can choose when collecting
@@ -16,27 +14,34 @@ public class getMaxAppleCount {
      */
     public static int getCount(Integer[] A, int K, int L) {
         int totalOfKAndL = 0;
-
+        int max1, max2;
         // maximum sum for Alice
         Integer[] maxAndStartingIndex = getMaxSumFromArray(A, K);
-        totalOfKAndL+=maxAndStartingIndex[0];
+        totalOfKAndL += maxAndStartingIndex[0];
         System.out.println(maxAndStartingIndex[0]);
 
-        //create an array with the remaining elements after the one with the maximum sum has been found out
+        //create 2 arrays with leading and trailing elements and find the max sum for remaining
         Integer[] leadingElements = Arrays.copyOfRange(A, 0, maxAndStartingIndex[1]);
-        Integer[] trailingElements = Arrays.copyOfRange(A, (maxAndStartingIndex[1]+K), A.length);
-        List<Integer> mergedLeadingAndTrailingElements = new ArrayList(Arrays.asList(leadingElements));
-        mergedLeadingAndTrailingElements.addAll(Arrays.asList(trailingElements));
-        Integer[] mergedLeadingAndTrailingElementsArray =  mergedLeadingAndTrailingElements.toArray(new Integer[0]);
+        List<Integer> LeadingElements = new ArrayList(Arrays.asList(leadingElements));
+        Integer[] LeadingElementsArray = LeadingElements.toArray(new Integer[0]);
+        max1 = getMaxSumFromArray(LeadingElementsArray, L)[0];
+        Integer[] trailingElements = Arrays.copyOfRange(A, (maxAndStartingIndex[1] + K), A.length);
+        List<Integer> TrailingElements = new ArrayList(Arrays.asList(trailingElements));
+        Integer[] trailingElementsArray = TrailingElements.toArray(new Integer[0]);
+        max2 = getMaxSumFromArray(trailingElementsArray, L)[0];
+        if (max1 > max2) {
+            maxAndStartingIndex[0] = max1;
+        } else {
+            maxAndStartingIndex[0] = max2;
+        }
 
-        //maximum sum for Bob
-        maxAndStartingIndex = getMaxSumFromArray(mergedLeadingAndTrailingElementsArray, L);
         System.out.println(maxAndStartingIndex[0]);
-        totalOfKAndL+=maxAndStartingIndex[0];
+        totalOfKAndL += maxAndStartingIndex[0];
 
         //If no combination is possible
-        if ((K + L) >=A.length)
+        if ((K + L) >= A.length) {
             return -1;
+        }
         return totalOfKAndL;
 
     }
@@ -65,10 +70,11 @@ public class getMaxAppleCount {
         maxAndStartingIndex[1] = startingIndex;
         return maxAndStartingIndex;
     }
+
     public static void main(String[] args) {
         int total;
         getMaxAppleCount g = new getMaxAppleCount();
-        Integer[] A = {6, 1, 4, 6,3,2,7,4};
+        Integer[] A = {6, 1, 4, 6, 3, 2, 7, 4};
         int K = 3;
         int L = 2;
         total = g.getCount(A, K, L);
